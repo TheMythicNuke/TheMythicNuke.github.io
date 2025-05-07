9d1840685dac56e1566c826f785941e8a108693c
TheMythicNuke <leo.sunnergrenedlund@skola.sala.se>
2025-05-07 10:33:22 +0200

    OXO

---
+++ IndexJS.js
@@ -0,0 +1,17 @@
+
+const images = [
+    `sowpis2.jpg`,
+    `sowpis4.jpg`,
+    `sowpis3.jpg`,
+    `sowpis1S,jpg`
+];
+
+let currentIndex = 0
+
+function changeImage() {
+    const slideshow = document.getElementById(`slideshow`);
+    currentIndex = (currentIndex + 1) % images.length;
+    slideshow.src = images[currentIndex];
+}
+
+setInterval(changeImage, 3000)
